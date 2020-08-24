f7466c1658fc8b7eb72b6547c0a6680a1b2af09d
Lucas Lima Oliveira <o.lucas.lima@gmail.com>
2020-08-24 15:37:54 -0300

    Getting won deals from pipedriver

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
+const app = express();
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/PipedriveToBling', {
@@ -10,5 +12,21 @@ mongoose.connect('mongodb://localhost:27017/PipedriveToBling', {
 });
 
 app.use(bodyParser.json());
+
+app.get('/pipedriver', function (req, res, next) {
+  const PIPEDRIVER_API_URL = process.env.PIPEDRIVER_API_URL;
+  const PIPEDRIVER_TOKEN = process.env.PIPEDRIVE_TOKEN;
+
+  axios
+    .get(`${PIPEDRIVER_API_URL}/deals`, { params: { api_token: PIPEDRIVER_TOKEN } })
+    .then((payload) => {
+      const onlyWonDeals = payload.data.data.filter((deal) => deal.status == 'won');
+      res.status(200).json({ deals: onlyWonDeals });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Couldnt conenct to pipedriver api' });
+    });
+});
+app.set('port', process.env.PORT || 3000);
 
 module.exports = app;
