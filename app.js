fb61dbd8ca531cceea7e67bfd9c16acd0f4fb1cc
Lucas Lima Oliveira <o.lucas.lima@gmail.com>
2020-08-24 19:39:12 -0300

    first version of xml to bling

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const { getDeals } = require('./deals.js');
+const builder = require('xmlbuilder');
+
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/PipedriveToBling', {
   useNewUrlParser: true,
@@ -24,11 +26,19 @@ app.get('/pipedriver', function (req, res, next) {
 
 app.post('/pipedriver', function (req, res, next) {
   let deal = req.body.current;
+  console.log(req.body);
   if (deal.status !== 'won') {
     console.log('Deal was updated but its not won yet');
   } else {
     console.log('The deal is won!');
+    const payloadToBlingJson = {
+      pedido: { cliente: { nome: deal.person_name } },
+    };
+    let payloadToBlingXML = builder.create('root');
+    payloadToBlingXML.ele('pedido').ele('cliente').txt(payloadToBlingJson.pedido.cliente.nome);
+    console.log(payloadToBlingXML.toString({ pretty: true }));
   }
+
   res.status(200).send('ok');
 });
 app.set('port', process.env.PORT || 3000);
