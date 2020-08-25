8d8d783f69bfe76358a3ed69e2a119d29ec8984d
Lucas Lima Oliveira <o.lucas.lima@gmail.com>
2020-08-25 00:07:39 -0300

    refactored code that uploads deal to Bling

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const qs = require('qs');
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const { getDeals } = require('./deals.js');
-const builder = require('xmlbuilder');
-const axios = require('axios');
-const { default: Axios } = require('axios');
-const { json } = require('body-parser');
+const { getDeals, uploadDealToBling } = require('./deals.js');
+
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://localhost:27017/PipedriveToBling', {
   useNewUrlParser: true,
@@ -29,11 +25,10 @@ app.get('/pipedriver', function (req, res, next) {
 
 app.post('/pipedriver', function (req, res, next) {
   let deal = req.body.current;
-  const BLING_API_URL = process.env.BLING_API_URL;
-  const BLING_TOKEN = process.env.BLING_TOKEN;
 
   if (deal.status !== 'won') {
     console.log('Deal was updated but its not won yet');
+    res.status(200);
   } else {
     console.log('The deal is won!');
     const payloadToBlingJson = {
@@ -42,36 +37,9 @@ app.post('/pipedriver', function (req, res, next) {
         cliente: { nome: deal.person_name },
       },
     };
-    let payloadToBlingXML = builder.create('root');
-    payloadToBlingXML
-      .ele('pedido')
-      .ele('cliente')
-      .txt(payloadToBlingJson.pedido.cliente.nome)
-      .up()
-      .ele('itens')
-      .ele('item')
-      .ele('descricao')
-      .txt(payloadToBlingJson.pedido.itens[0].descricao)
-      .up()
-      .ele('vlr_unit')
-      .txt(payloadToBlingJson.pedido.itens[0].vlr_unit)
-      .up()
-      .ele('codigo')
-      .txt(payloadToBlingJson.pedido.itens[0].codigo);
-    console.log(payloadToBlingXML.toString({ pretty: true }));
-    const data = { xml: payloadToBlingXML.toString(), apikey: BLING_TOKEN };
-    const options = {
-      method: 'POST',
-      headers: { 'content-type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(data),
-      url: BLING_API_URL,
-    };
-    axios(options)
-      .then((resBling) => {
-        console.log(JSON.stringify(resBling.data));
-        res.status(200).send('ok');
-      })
-      .catch((err) => res.status(500));
+    uploadDealToBling(payloadToBlingJson)
+      .then((responseFromBling) => res.status(200).send('ok'))
+      .catch((responseFromBlingError) => res.status(500).send('Error sendind data to Bling'));
   }
 });
 app.set('port', process.env.PORT || 3000);
--- deals.js
+++ deals.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 const PIPEDRIVER_API_URL = process.env.PIPEDRIVER_API_URL;
 const PIPEDRIVER_TOKEN = process.env.PIPEDRIVE_TOKEN;
+const BLING_API_URL = process.env.BLING_API_URL;
+const BLING_TOKEN = process.env.BLING_TOKEN;
+const builder = require('xmlbuilder');
+const qs = require('qs');
 
 async function getDeals(filterFunction = null) {
   let responseFromPipeDriver = await axios.get(`${PIPEDRIVER_API_URL}/deals`, {
@@ -13,5 +17,37 @@ async function getDeals(filterFunction = null) {
     return allDeals;
   }
 }
-
-module.exports = { getDeals };
+function jsonToXML(jsonPayload) {
+  let payloadToBlingXML = builder.create('root');
+  payloadToBlingXML
+    .ele('pedido')
+    .ele('cliente')
+    .txt(jsonPayload.pedido.cliente.nome)
+    .up()
+    .ele('itens')
+    .ele('item')
+    .ele('descricao')
+    .txt(jsonPayload.pedido.itens[0].descricao)
+    .up()
+    .ele('vlr_unit')
+    .txt(jsonPayload.pedido.itens[0].vlr_unit)
+    .up()
+    .ele('codigo')
+    .txt(jsonPayload.pedido.itens[0].codigo);
+  return payloadToBlingXML.toString();
+}
+async function uploadDealToBling(jsonPayload) {
+  const data = { xml: jsonToXML(jsonPayload), apikey: BLING_TOKEN };
+  const options = {
+    method: 'POST',
+    headers: { 'content-type': 'application/x-www-form-urlencoded' },
+    data: qs.stringify(data),
+    url: BLING_API_URL,
+  };
+  responseFromBling = await axios(options);
+  if (responseFromBling.data.retorno.erros) {
+    throw responseFromBling;
+  }
+  return responseFromBling;
+}
+module.exports = { getDeals, uploadDealToBling };
